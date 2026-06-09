dd8fa881753e8795b11dadf8b5640bebeb77c575
dakshpant <dakshpant15@gmail.com>
2026-06-09 17:58:13 +0530

    Profile caching using reedis

--- 05-user-profile-cache-json-vs-hash/src/index.js
+++ 05-user-profile-cache-json-vs-hash/src/index.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import Redis from 'ioredis';
+import express from "express";
+import Redis from "ioredis";
 
 const app = express();
 app.use(express.json());
 
-const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
-app.post("/user/:id/json", async(req,res)=>{
-    await redis.set(`user:${req.params.id}`, JSON.stringify(req.body));
-    res.json({savedAs:`Json`});
-})
+app.post("/user/:id/json", async (req, res) => {
+  await redis.set(`user:${req.params.id}:json`, JSON.stringify(req.body));
+  res.json({ savedAs: `Json`, user: req.body });
+});
 
-app.get("/user/:id/json", async(req, res)=>{
-    const raw = await redis.get(`user:${req.params.id}:json`);
+app.get("/user/:id/json", async (req, res) => {
+  const raw = await redis.get(`user:${req.params.id}:json`);
 
-    res.json({
-        user: raw ? JSON.parse(raw) : null
-    })
-})
+  res.json({
+    user: raw ? JSON.parse(raw) : null,
+  });
+});
 
 /**
  * set -> store single variable
@@ -25,16 +25,16 @@ app.get("/user/:id/json", async(req, res)=>{
  * hgetall -> takes and updates and returns a hash as an object
  */
 
-app.post("/user/:id/hash", async(req, res) =>{
-    await redis.hset(`user:${req.params.id}:hash`, req.body)
-    res.json({savedAs:"hash"})
-})
+app.post("/user/:id/hash", async (req, res) => {
+  await redis.hset(`user:${req.params.id}:hash`, req.body);
+  res.json({ savedAs: "hash" });
+});
 
-app.get("/user/:id/hash", async(req, res)=>{
-     const user = await redis.hgetall(`user:${req.params.id}:hash`);
-     res.json({user});
-})
+app.get("/user/:id/hash", async (req, res) => {
+  const user = await redis.hgetall(`user:${req.params.id}:hash`);
+  res.json({ user });
+});
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
